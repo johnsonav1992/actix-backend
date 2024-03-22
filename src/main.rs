69cee9403970eadbb2d8df99c978d3b4634afc5a
Alex Johnson <johnsonav1992@gmail.com>
2024-03-22 13:56:02 -0500

    got a dummy endpoint!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{App, HttpServer};
+use actix_web::{ App, HttpServer, Responder};
+
+#[actix_web::get("/greet")]
+async fn greet() -> impl Responder {
+    format!("Hey there!")
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
+    println!("Server running on port {}", port);
 
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().service(greet))
         .bind(("127.0.0.1", port))?
         .workers(2)
         .run()
