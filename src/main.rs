bf0eea361e2ae36c528f8298a5e822c055b66426
Alex Johnson <johnsonav1992@gmail.com>
2024-03-22 14:05:40 -0500

    setting up dummy db

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use actix_web::{ App, HttpServer, Responder};
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
-#[actix_web::get("/greet")]
-async fn greet() -> impl Responder {
-    format!("Hey there!")
+use actix_web::{ App, HttpServer, Responder, web};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    name: String
+}
+
+type UserDb = Arc<Mutex<HashMap<u32, User>>>;
+
+#[actix_web::get("/greet/{id}")]
+async fn greet(user_id: web::Path<u32>) -> impl Responder {
+    format!("Hey there, {}!", user_id)
 }
 
 #[actix_web::main]
@@ -10,7 +20,12 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("Server running on port {}", port);
 
-    HttpServer::new(|| App::new().service(greet))
+    let user_db: UserDb = Arc::new(Mutex::new(HashMap::<u32, User>::new()));
+
+    HttpServer::new(move || {
+        let app_data = web::Data::new(user_db.clone());
+        App::new().service(greet)
+    })
         .bind(("127.0.0.1", port))?
         .workers(2)
         .run()
