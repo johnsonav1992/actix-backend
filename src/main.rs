9e9d0cb78c7f6ea278379c8a0e64ef9846ae3254
Alex Johnson <johnsonav1992@gmail.com>
2024-03-22 13:52:19 -0500

    server set up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+
+    HttpServer::new(|| App::new())
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
 }
