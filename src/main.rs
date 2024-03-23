a26ffa574db41524949b461521170f38884b750a
Alex Johnson <johnsonav1992@gmail.com>
2024-03-23 06:07:03 -0500

    got get and post done!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, sync::{Arc, Mutex}};
 
-use actix_web::{ web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
+use actix_web::{ error::ErrorNotFound, web, App, HttpResponse, HttpServer, Responder, Error};
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -10,21 +10,48 @@ struct User {
 
 type UserDb = Arc<Mutex<HashMap<u32, User>>>;
 
-#[actix_web::get("/greet/{id}")]
-async fn greet(user_id: web::Path<u32>) -> impl Responder {
-    format!("Hey there, {}!", user_id)
+#[actix_web::get("/users/{id}")]
+async fn get_user(
+    user_id: web::Path<u32>
+    , db: web::Data<UserDb>
+) -> Result<impl Responder, Error> {
+    let user_id = user_id.into_inner();
+    let db = db.lock().unwrap();
+
+    match db.get(&user_id) {
+        Some(user_data) => Ok(HttpResponse::Ok().json(user_data)),
+        None => Err(ErrorNotFound("User not found")),
+    }
+}
+
+#[actix_web::get("/users")]
+async fn get_users( db: web::Data<UserDb> ) -> impl Responder {
+    let db = db.lock().unwrap();
+
+    let users: Vec<&User> = db.values().collect();
+
+    HttpResponse::Ok().json(users)
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateUSerResponse {
+    id: u32
+    , name: String
 }
 
 #[actix_web::post("/users")]
-async fn create_user(user_data: web::Json<User>, db: web::Data<UserDb>) 
-    -> impl Responder {
+async fn create_user(
+    user_data: web::Json<User>
+    , db: web::Data<UserDb>
+) -> impl Responder {
         let mut db = db.lock().unwrap();
-        let new_id: db.keys().max().unwrap_or(&0) + 1;
+        let new_id = db.keys().max().unwrap_or(&0) + 1;
         let name = user_data.name.clone();
 
         db.insert(new_id, user_data.into_inner());
         
-        HttpResponse::Created().json(User {
+        HttpResponse::Created().json(CreateUSerResponse {
+            id: new_id,
             name
         })
 }
@@ -38,7 +65,10 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         let app_data = web::Data::new(user_db.clone());
-        App::new().app_data(app_data).service(greet)
+        App::new().app_data(app_data)
+        .service(get_user)
+        .service(create_user)
+        .service(get_users)
     })
         .bind(("127.0.0.1", port))?
         .workers(2)
